97a9f198888279b16f1ceb9808d346e0a5ead6d6
Tal Ben-Ari <t-tabena@microsoft.com>
2017-07-24 11:24:44 -0700

    Set up a simple server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,14 @@
-console.log('hello')
+import * as express from 'express'
+import * as winston from 'winston'
+
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('*', (req, res) => {
+  res.send('Hello, world!')
+})
+
+const server = app.listen(app.get('port'), () => {
+  winston.info(`Server listening on port ${server.address().port}`)
+})
