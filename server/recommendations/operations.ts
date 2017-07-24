0827d1d617143aa5ac6d63f5a7186e3684243892
Tal Ben-Ari <t-tabena@microsoft.com>
2017-07-24 15:51:10 -0700

    Write cancelOperation

--- server/recommendations/operations.ts
+++ server/recommendations/operations.ts
@@ -7,6 +7,14 @@ const { subscriptionKey: SUBSCRIPTION_KEY } = recommendations
 const baseURL = (id: string) =>
   `https://westus.api.cognitive.microsoft.com/recommendations/v4.0/operations/${id}`
 
+export const cancelOperation = async <T>(
+  id: string
+): Promise<OperationInfo<T> | ErrorResponse> =>
+  (await request
+    .delete(baseURL(id))
+    .set('Ocp-Apim-Subscription-Key', SUBSCRIPTION_KEY)
+    .send()).body
+
 export const getOperation = async <T>(
   id: string
 ): Promise<OperationInfo<T> | ErrorResponse> =>
