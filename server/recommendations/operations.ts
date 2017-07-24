4f0a28ec0704b9ba8d6a4cc9a6bbba14d34ed2be
Tal Ben-Ari <t-tabena@microsoft.com>
2017-07-24 15:55:06 -0700

    Rename id to operationId

--- server/recommendations/operations.ts
+++ server/recommendations/operations.ts
@@ -4,21 +4,21 @@ import { ErrorResponse, OperationInfo } from './types'
 
 const { subscriptionKey: SUBSCRIPTION_KEY } = recommendations
 
-const baseURL = (id: string) =>
-  `https://westus.api.cognitive.microsoft.com/recommendations/v4.0/operations/${id}`
+const baseURL = (operationId: string) =>
+  `https://westus.api.cognitive.microsoft.com/recommendations/v4.0/operations/${operationId}`
 
 export const cancelOperation = async <T>(
-  id: string
+  operationId: string
 ): Promise<OperationInfo<T> | ErrorResponse> =>
   (await request
-    .delete(baseURL(id))
+    .delete(baseURL(operationId))
     .set('Ocp-Apim-Subscription-Key', SUBSCRIPTION_KEY)
     .send()).body
 
 export const getOperation = async <T>(
-  id: string
+  operationId: string
 ): Promise<OperationInfo<T> | ErrorResponse> =>
   (await request
-    .get(baseURL(id))
+    .get(baseURL(operationId))
     .set('Ocp-Apim-Subscription-Key', SUBSCRIPTION_KEY)
     .send()).body
